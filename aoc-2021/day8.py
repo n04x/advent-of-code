efcf3ddb0deebe329d7b8dd9e7a1dc6583796a70
noax <thomasbacks@gmail.com>
2021-12-15 22:17:00 -0500

    started day 8

--- aoc-2021/day8.py
+++ aoc-2021/day8.py
@@ -1,8 +1,16 @@
 #region modules
+import re
 #endregion
 
 #region functions
+
 #endregion
 
 #region main
+with open('./input/test.txt') as file:
+    data = re.findall(r'(.*) \| (.*)', file.read())
+    data = [(ins.split(), outs.split()) for ins, outs in data]
+
+uniques = (2, 3, 4, 7)
+    
 #endregion
