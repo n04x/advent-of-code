f3aea777fba462d2d90548c23d38343417c076cb
noax <thomasbacks@gmail.com>
2021-12-19 09:15:11 -0500

    merge both part for day 2 and rename day2.py to not mess with ordering

--- aoc-2021/day02.py
+++ aoc-2021/day02.py
@@ -18,21 +18,33 @@ class Direction:
 
     def CurrentPosition(self):
         print('Submarine current position are {} units horizontal, {} units depth.'.format(self.forward,self.depth))
-    
+
     def Answer(self):
         return self.forward * self.depth
 
 
 # Initialize submarine position
-submarine_pos = Direction(0,0,0)
+submarine_pos1 = Direction(0, 0, 0)
+submarine_pos2 = Direction(0,0,0)
+# Part 1
+for l in lines:
+    data = l.split(' ')
+    if data[0] == 'forward':
+        submarine_pos1.forward += int(data[1])
+    elif data[0] == 'up':
+        submarine_pos1.depth -= int(data[1])
+    elif data[0] == 'down':
+        submarine_pos1.depth += int(data[1])
+
+# Part 2
 for line in lines:
     data = line.split(' ')
     if data[0] == 'forward':
-        submarine_pos.MoveForward(int(data[1]))
+        submarine_pos2.MoveForward(int(data[1]))
     elif data[0] == 'up':
-        submarine_pos.SetAim(-abs(int(data[1])))
+        submarine_pos2.SetAim(-abs(int(data[1])))
     elif data[0] == 'down':
-        submarine_pos.SetAim(int(data[1]))
+        submarine_pos2.SetAim(int(data[1]))
 
-submarine_pos.CurrentPosition()
-print('Multiplying forward position and current depth, we get {}'.format(submarine_pos.Answer()))
+print('Part #1 Answer: {}'.format(submarine_pos1.Answer()))
+print('Part #2 Answer: {}'.format(submarine_pos2.Answer()))
