d7e3722496d83ea7008fad6b5e8dc78d2740bde5
n04x <ThomasBacks@gmail.com>
2023-12-13 11:39:22 -0500

    Solved Day 07 part 2

--- aoc-2023/Day07/day07.ps1
+++ aoc-2023/Day07/day07.ps1
@@ -2,6 +2,7 @@
 $TESTING = $false
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 $cardList = @("A","K","Q","J","T","9","8","7","6","5","4","3","2") # T is b, J is c, Q is d, K is e, A is f
+$cardListWildJ = @("A","K","Q","T","9","8","7","6","5","4","3","2") # T is b, Q is d, K is e, A is f
 #endregion
 
 #region Functions
@@ -21,6 +22,24 @@ function Get-Data {
     return $result
 }
 
+function Get-DataWildJ {
+    param ($d)
+    $result = @()
+    foreach($line in $d) {
+        $lineArray = $line.Split(" ")
+        $props = [ordered]@{
+            Hand = $lineArray[0]
+            Joker = ($lineArray[0].ToCharArray() | Where-Object {$_ -eq 'J'}).Count
+            CardValue = $lineArray[0].Replace('T','b').Replace('J','0').Replace('Q','d').Replace('K','e').Replace('A','f')
+            Bid = [int]$lineArray[1]
+            Rank = 0
+            CardCount = 0
+        }
+        $result += New-Object -TypeName psobject -Property $props
+    }
+    return $result
+}
+
 function Get-HandRank {
     param($hands)
     foreach($hand in $hands) {
@@ -30,19 +49,33 @@ function Get-HandRank {
         }
         
         if($cardCount -contains 5) { $hand.Rank = 6 } # 5 of a kind
-        # if($cardCount -contains 5) { $hand.Rank = "6" + $hand.CardValue } # 5 of a kind
         elseif($cardCount -contains 4) { $hand.Rank = 5} # 4 of a kind
-        # elseif($cardCount -contains 4) { $hand.Rank = "5" + $hand.CardValue } # 4 of a kind
         elseif(($cardCount -contains 3) -and ($cardCount -contains 2)) { $hand.Rank = 4 }# full house
-        # elseif(($cardCount -contains 3) -and ($cardCount -contains 2)) { $hand.Rank = "4" + $hand.CardValue }# full house
-        elseif($cardCount -contains 3) { $hand.Rank = 3 } # Three of a kind
-        # elseif($cardCount -contains 3) { $hand.Rank = "3" + $hand.CardValue } # Three of a kind
-        elseif(($cardCount | Where-Object {$_ -eq 2}).Count -eq 2) { $hand.Rank = 2 } # Two pairs
-        # elseif(($cardCount | Where-Object {$_ -eq 2}).Count -eq 2) { $hand.Rank = "2" + $hand.CardValue } # Two pairs
-        elseif(($cardCount | Where-Object {$_ -eq 2}).Count -eq 1) { $hand.Rank = 1 } # One pair
-        # elseif(($cardCount | Where-Object {$_ -eq 2}).Count -eq 1) { $hand.Rank = "1" + $hand.CardValue } # One pair
-        else {$hand.Rank = 0 }
+        elseif($cardCount -contains 3) { $hand.Rank = 3 } # 3 of a kind
+        elseif(($cardCount | Where-Object {$_ -eq 2}).Count -eq 2) { $hand.Rank = 2 } # 2 pairs
+        elseif(($cardCount | Where-Object {$_ -eq 2}).Count -eq 1) { $hand.Rank = 1 } # 1 pair
+        else { $hand.Rank = 0 }
     }
+}
+
+function Get-HandRankWildJ {
+    param ($hands)       
+        foreach($hand in $hands) {
+            $cardCountArray = @()
+            foreach($card in $cardListWildJ) {
+                $cardCountArray += (($hand.Hand).ToCharArray() | Where-Object {$_ -eq $card} | Measure-Object).Count
+            }
+            $cardCount = ($cardCountArray | Sort-Object -Descending | Select-Object -First 1) + $hand.Joker
+            $hand.CardCount = $cardCount
+            if($cardCount -eq 5) { $hand.Rank = 6 } # 5 of a kind
+            elseif($cardCount -eq 4) { $hand.Rank = 5 } # 4 of a kind
+            elseif((($cardCountArray | Where-Object { $_ -eq 2 }).Count -eq 2) -and ($hand.Joker -eq 1)) { $hand.Rank = 4 } # full house with Joker   
+            elseif(($cardCountArray -contains 3) -and ($cardCountArray -contains 2)) { $hand.Rank = 4 } # full house without Joker
+            elseif($cardCount -eq 3) { $hand.Rank = 3 } # 3 of a kind
+            elseif((($cardCountArray | Where-Object { $_ -eq 2 }).Count -eq 2)) { $hand.Rank = 2 } # 2 pairs
+            elseif($cardCount -eq 2) { $hand.Rank = 1 } # 1 pair
+            else{ $hand.Rank = 0 }
+        }
 }
 
 function Get-TotalWinnings {
@@ -62,9 +95,15 @@ if($TESTING) {
     $data = Get-Content "$scriptPath\input.txt"
 }
 
+# Part one solution
 $hands = Get-Data $data
 Get-HandRank $hands
 $handsSorted = $hands | Sort-Object Rank, CardValue
 Write-Host "The answer for the part one is $(Get-TotalWinnings $handsSorted)" -ForegroundColor Green
 
+# Part two solution
+$handsWithJoker = Get-DataWildJ $data
+Get-HandRankWildJ $handsWithJoker
+$handsWithJokerSorted = $handsWithJoker | Sort-Object Rank, CardValue
+Write-Host "The answer for the part two is $(Get-TotalWinnings $handsWithJokerSorted)" -ForegroundColor Green
 #endregion
