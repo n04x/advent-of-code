5f46abe6d174562026d599c32e70d7a58f5a37d6
noax <thomasbacks@gmail.com>
2023-12-14 00:52:00 -0500

    Started Part 2 for Day 08

--- aoc-2023/Day08/day08.ps1
+++ aoc-2023/Day08/day08.ps1
@@ -1,5 +1,5 @@
 #region Parameters
-$TESTING = $false
+$TESTING = $true
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 $nodes = @()
 #endregion
@@ -12,8 +12,16 @@ function Get-Data {
     $instructions = $dataArray[0]
     for($i = 1; $i -lt $dataArray.Count; $i++) {
         $elements = $dataArray[$i].Split('=')[1].Replace('(','').Replace(')','').Split(',').Trim()
+        if($dataArray[$i].Split("=")[0].Trim() -like "*A") {
+            $type = "Start"
+        } elseif ($dataArray[$i].Split("=")[0].Trim() -like "*Z") {
+            $type = "End"
+        } else {
+            $type = "Mid"
+        }
         $props = [ordered]@{
             Node = $dataArray[$i].Split("=")[0].Trim()
+            Type = $type
             Left = $elements[0]
             Right = $elements[1]
         }
@@ -55,11 +63,18 @@ function Get-Steps {
 #endregion
 
 #region Script
-if($TESTING) { $data = Get-Content "$scriptPath\test.txt" } else { $data = Get-Content "$scriptPath\input.txt" }
+if($TESTING) { 
+    $data = Get-Content "$scriptPath\test.txt" 
+    $data_P2Test = Get-Content "$scriptPath\testp2.txt"
+} else { 
+    $data = Get-Content "$scriptPath\input.txt" 
+}
 
 $instructions, $nodes = Get-Data $data
+$nodes | Format-Table
 $answer_p1 = Get-Steps $instructions $nodes
 Write-Host "The answer for the part one is $answer_p1" -ForegroundColor Green
 
-
+# Below are information used for testing purpose only.
+$nodes | Format-Table
 #endregion
