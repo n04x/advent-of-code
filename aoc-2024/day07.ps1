d10050365c5edf3c334fe51eea77652cdf185230
n04x <ThomasBacks@gmail.com>
2024-12-16 08:29:45 -0500

    Update day07.ps1
    Solved day 2 of AoC, simply by adding a new variable to the function to reflect the change.

--- aoc-2024/day07.ps1
+++ aoc-2024/day07.ps1
@@ -4,6 +4,7 @@ $title = "Day 7: Bridge Repair"
 $testing = $false
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 [long]$sumOfValidEquation = 0
+[long]$sumOfValidEquationWithConcat = 0
 #endregion
 
 #region Modules
@@ -15,7 +16,7 @@ Import-Module -Name "$scriptPath/modules/WriteOutcome.psm1" -Force
 
 #endregion 
 function Get-ValidEquation {
-    param($eq)
+    param($eq, $useConcat)
     [long]$result = $eq.Split(':')[0].Trim() # use long instead since there's some value that are larger than int.
     $statement = $eq.Split(':')[1].Trim().Split(' ')
 
@@ -28,26 +29,32 @@ function Get-ValidEquation {
 
         [long]$add = $currentNumber + $statement[$index]
         [long]$multi = $currentNumber * $statement[$index]
+        [long]$concat = [string]$currentNumber + [string]$statement[$index]
 
         if($index -eq $statement.Count - 1) {
             if($add -eq $result) {
-                Write-Host $qStatement -ForegroundColor Cyan
                 return $add
             } elseif($multi -eq $result) {
-                Write-Host $qStatement -ForegroundColor Cyan
                 return $multi
+            } elseif($useConcat -and ($concat -eq $result)) {
+                return $concat
             }
         } else {
             if($add -le $result) { $qStatement.Add(@($index,$add)) | Out-Null }
             if($multi -le $result) { $qStatement.Add(@($index,$multi)) | Out-Null }
+            if($useConcat -and ($concat -le $result)) { $qStatement.Add(@($index,$concat)) | Out-Null }
         }
     }
 }
+
 #region Main
 $data = Open-DataFile -DayNumber $day -Testing $testing -ScriptPath $scriptPath
 foreach($equation in $data) {
-    $sumOfValidEquation += Get-ValidEquation $equation
+    Write-Host $equation -ForegroundColor Cyan
+    $sumOfValidEquation += Get-ValidEquation $equation $false
+    $sumOfValidEquationWithConcat += Get-ValidEquation $equation $true
 }
 Write-Answer -Title $title -Testing $testing -ExpectedAnswer 3749 -Answer $sumOfValidEquation -Part 1
+Write-Answer -Title $title -Testing $testing -ExpectedAnswer 11387 -Answer $sumOfValidEquationWithConcat -Part 2
 
 #endregion
